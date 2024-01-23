01542dccdc198acc5903372131bbf55492fade7d
ardapelkroke <noizjansen@gmail.com>
2024-01-23 12:26:57 +0100

    Added method to convert to vector

--- card.py
+++ card.py
@@ -3,11 +3,15 @@ import os
 
 from pygame.locals import RLEACCEL
 
+colors= ["red", "green", "purple"]
+shapes= ["squiggle", "oval", "diamond"]
+fills= ["empty", "filled", "shaded"]
+numbers= ["1", "2", "3"]
 
 class Card(pygame.sprite.Sprite):
     def __init__(self, color, shape, fill, number):
         super(Card, self).__init__()
-        self.surf = pygame.image.load(os.path.dirname(os.path.realpath(__file__)) + "\\kaarten\\" + color + shape + fill + number + ".gif")
+        self.surf = pygame.image.load(os.path.dirname(os.path.realpath(__file__)) + "/kaarten/" + color + shape + fill + number + ".gif")
         self.rect = self.surf.get_rect()
         self._color=color
         self._shape=shape
@@ -18,23 +22,26 @@ class Card(pygame.sprite.Sprite):
         return self._color, self._shape, self._fill, self._number 
     
     def color(self, color):
-        if color in ["red", "green", "purple"]:
+        if color in colors:
             self._color=color
         else: return False
 
     def shape(self, shape):
-        if shape in ["squiggle", "oval", "diamond"]:
+        if shape in shapes:
             self._shape=shape
         else: return False
 
     def fill(self, fill):
-        if fill in ["empty", "filled", "shaded"]:
+        if fill in fills:
             self._fill=fill
         else: return False
 
     def number(self, number):
-        if number in ["1", "2", "3"]:
+        if number in numbers:
             self._number=number
         else: return False
+    
+    def toVector(self):
+        return [colors.index(self._color)+1, shapes.index(self._shape)+1, fills.index(self._fill)+1, numbers.index(self._number)+1]
 
 
